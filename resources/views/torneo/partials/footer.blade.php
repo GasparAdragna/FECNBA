9957287b3b0685f5d3bfdd32e1541d410e73fe33
GasparAdragna <gaspar.jac@hotmail.com>
2023-05-23 14:08:24 -0300

    Actualiza el reglamento 2023

--- resources/views/torneo/partials/footer.blade.php
+++ resources/views/torneo/partials/footer.blade.php
@@ -2,10 +2,13 @@
     <div class="row bg-dark-blue d-flex align-items-center footer">
         <div class="col-md-4 text-center">
             <h4 class="text-white">FECNBA</h4>
-            <a class="text-white" href="https://docs.google.com/document/d/e/2PACX-1vT_Zs_iZk2ajXywVtfdyPFfk3nRibXxZUT_e-ZFuXjwgV6CA6-0CfC1kEVD4RWRPQ/pub" target="_blank">Reglamento 2022</a>
+            <a class="text-white"
+                href="https://docs.google.com/document/d/15_J69jrX4hDGB8fzL1z2iQQ0wAJsvedFbiPMyxt39FM/edit?usp=sharing"
+                target="_blank">Reglamento 2023</a>
         </div>
         <div class="col-md-4 text-white text-center">
-            <p>Copyright <b>FECNBA</b> todos los derechos reservados. Una página de <a href="https://www.linkedin.com/in/gaspar-adragna/" target="_blank">Gaspar Adragna</a></p>
+            <p>Copyright <b>FECNBA</b> todos los derechos reservados. Una página de <a
+                    href="https://www.linkedin.com/in/gaspar-adragna/" target="_blank">Gaspar Adragna</a></p>
         </div>
         <div class="col-md-4 text-center text-white iconos">
             <i class="fab fa-twitter fs-1"></i>
